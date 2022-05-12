645a4bddfaca5ba20dec66498c465683eb4d4e7b
Lin Liu <liulinn60@gmail.com>
2022-05-11 19:10:39 -0700

    sign up function with firebase

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,19 +1,78 @@
 import './signUp.scss'
+import { useState } from 'react'
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    updateProfile,
+} from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../../firebase.config'
+import { useNavigate } from 'react-router-dom'
 
 function SignUp() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const { email, password } = formData
+
+    const navigate = useNavigate()
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }))
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            )
+
+            const user = userCredential.user
+
+            const formDataCopy = { ...formData }
+            delete formDataCopy.password
+            formDataCopy.timestamp = serverTimestamp()
+
+            await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className='signUp'>
             <div className='signUpContent'>
                 <h1 className='title'>Sign Up</h1>
 
-                <form className='signUpForm'>
+                <form onSubmit={onSubmit}>
                     <label htmlFor='Email'>Email</label>
-                    <input type='text' id='email' placeholder='Email' />
+                    <input
+                        type='text'
+                        id='email'
+                        value={email}
+                        placeholder='Email'
+                        onChange={onChange}
+                    />
                     <label htmlFor='password'>Password</label>
                     <input
                         type='password'
                         id='password'
+                        value={password}
                         placeholder='Password'
+                        onChange={onChange}
                     />
                     <button className='signUpBtn'>Sign Up</button>
                 </form>
